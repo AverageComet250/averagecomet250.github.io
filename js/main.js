755a421591e98c1c2ab306ebf4ed0346a3453ef3
Shravan Mandava <shravan@mandava8.com>
2022-08-21 14:18:52 +0000

    implement base for iframe based page viewer

---
+++ js/main.js
@@ -0,0 +1,16 @@
+getQueryStringParams = query => {
+    return query
+        ? (/^[?#]/.test(query) ? query.slice(1) : query)
+            .split('&')
+            .reduce((params, param) => {
+                    let [key, value] = param.split('=');
+                    params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+                    return params;
+                }, {}
+            )
+        : {}
+};
+
+var iframe = document.getElementsByClassName("page-viewer")[0];
+var params = getQueryStringParams();
+
